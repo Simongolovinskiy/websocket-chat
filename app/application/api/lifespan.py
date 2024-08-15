0b58b86399c3f2d71548aa3a1d730829d3e37595
wasa <tositrent164@gmail.com>
2024-08-15 21:03:47 +0300

    remove custom healthcheck in code, add healthcheck in yaml files :fire:

--- app/application/api/lifespan.py
+++ app/application/api/lifespan.py
@@ -1,8 +1,5 @@
-import asyncio
-
 from contextlib import asynccontextmanager
 
-from aiokafka.errors import KafkaConnectionError
 
 from app.infrastructure.message_brokers.base import BaseMessageBroker
 from app.services.init import init_container
@@ -11,15 +8,7 @@ from app.services.init import init_container
 async def start_kafka(retries=5, delay=3):
     container = init_container()
     message_broker: BaseMessageBroker = container.resolve(BaseMessageBroker)
-    for attempt in range(retries):
-        try:
-            await message_broker.start()
-            break
-        except KafkaConnectionError as e:
-            if attempt < retries - 1:
-                await asyncio.sleep(delay)
-            else:
-                raise e
+    await message_broker.start()
 
 
 async def stop_kafka():
